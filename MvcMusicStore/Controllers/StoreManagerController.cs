73928ed71eb38d9957a63c3ca6eebb6c067d648c
Emmanuel Villatoro <kirst418@outlook.com>
2022-11-10 19:13:51 -0600

    Add project files.

---
+++ MvcMusicStore/Controllers/StoreManagerController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class StoreManagerController : Controller
+    {
+        private MusicStoreEntities db = new MusicStoreEntities();
+
+        // GET: StoreManager
+        public async Task<ActionResult> Index()
+        {
+            var albums = db.Albums.Include(a => a.Genre).Include(a=> a.Artist);
+            return View(await albums.ToListAsync());
+        }
+
+        // GET: StoreManager/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = await db.Albums.FindAsync(id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            await db.Entry(album).Reference(s => s.Genre).LoadAsync();
+            await db.Entry(album).Reference(s => s.Artist).LoadAsync();
+            return View(album);
+        }
+
+        // GET: StoreManager/Create
+        public ActionResult Create()
+        {
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name");
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name");
+            return View();
+        }
+
+        // POST: StoreManager/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "AlbumId,ArtistId,GenreId,Title,Price,AlbumArtUrl")] Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Albums.Add(album);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", album.GenreId);
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", album.ArtistId);
+            return View(album);
+        }
+
+        // GET: StoreManager/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = await db.Albums.FindAsync(id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", album.GenreId);
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", album.ArtistId);
+
+            return View(album);
+        }
+
+        // POST: StoreManager/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "AlbumId,ArtistId,GenreId,Title,Price,AlbumArtUrl")] Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(album).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", album.GenreId);
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", album.ArtistId);
+
+            return View(album);
+        }
+
+        // GET: StoreManager/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = await db.Albums.FindAsync(id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            await db.Entry(album).Reference(s => s.Genre).LoadAsync();
+            await db.Entry(album).Reference(s => s.Artist).LoadAsync();
+            return View(album);
+        }
+
+        // POST: StoreManager/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Album album = await db.Albums.FindAsync(id);
+            db.Albums.Remove(album);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcMusicStore/Models/MusicStoreEntities.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MvcMusicStore.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+    public class MusicStoreEntities: IdentityDbContext<ApplicationUser>
+    {
+        public MusicStoreEntities():base("MusicStoreEntities")
+        {
+
+        }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+
+        public static MusicStoreEntities Create()
+        {
+            return new MusicStoreEntities();
+        }
+
+    }
+}
